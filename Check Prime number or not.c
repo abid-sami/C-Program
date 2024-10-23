986fc2a86af3331830a85ed9a613d482006cfcc2
Ragib Hasan Abid <ragib7264@gmail.com>
2024-10-23 21:19:25 +0600

    Create Check Prime number or not.c

---
+++ Check Prime number or not.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main() {
+    int i, n, f=1;
+    scanf("%d", &n);
+    if (n<2)
+        printf("Invalid");
+    else
+    {
+        for (i=2; i<=n/2; i++)
+        {
+            if (n%i==0)
+            {
+                f=0;
+                break;
+            }
+        }
+        if (f==1)
+
+            printf("This is a prime number");
+        else
+            printf("This is not a prime number");
+    }
+    return 0;
+}
